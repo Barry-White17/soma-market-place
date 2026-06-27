0c8521231cb0796af3f0a85bc84c6b664d3f8333
Barry-White17 <whitwb2@gmail.com>
2026-06-27 04:29:09 +0100

    Update BACKEND_URL and SOCKET_URL for production

--- client/config.js
+++ client/config.js
@@ -4,8 +4,8 @@ const config = {
     stripe_connect_test_client_id:
         import.meta.env.STRIPE_CONNECT_TEST_CLIENT_ID ||
         'YOUR_STRIPE__CLIENT_ID',
-    BACKEND_URL: import.meta.env.BACKEND_URL || 'http://localhost:3000',
-    SOCKET_URL: import.meta.env.SOCKET_URL || 'localhost:3000',
+    BACKEND_URL: import.meta.env.BACKEND_URL || 'https://soma-market-place-backend.vercel.app',
+    SOCKET_URL: import.meta.env.SOCKET_URL || 'https://soma-merket-place-backend.vercel.app',
 }
 
 export default config
